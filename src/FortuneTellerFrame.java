378fceac9e6edacb76911a8011c8e8aa02c484b0
askarbolot <kabylbekov07@gmail.com>
2025-09-25 11:57:47 -0400

    Added some color changes

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -54,6 +54,7 @@ public class FortuneTellerFrame extends JFrame {
 
         createBottomPanel();
         add(mainPnl);
+        mainPnl.setBackground(new Color(173,216,230));
     }
 
     public void createTopPanel() {
@@ -67,6 +68,7 @@ public class FortuneTellerFrame extends JFrame {
 
          topPnl.add(titleLbl);
         mainPnl.add(topPnl, BorderLayout.NORTH);
+        topPnl.setBackground(new Color(173,216,230));
     }
 
     public void createCenterPanel() {
@@ -76,6 +78,7 @@ public class FortuneTellerFrame extends JFrame {
         displayTA.setEditable(false);
         centerPnl.add(scrollPane);
         mainPnl.add(centerPnl, BorderLayout.CENTER);
+        centerPnl.setBackground(new Color(173,216,230));
     }
 
     public void createBottomPanel() {
@@ -93,10 +96,20 @@ public class FortuneTellerFrame extends JFrame {
             curIdx = newIdx;
         });
         quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
+        Font newFont = new Font("Arial",Font.BOLD,30);
+        readFortuneBtn.setFont(newFont);
+        quitBtn.setFont(newFont);
+        readFortuneBtn.setBackground(Color.ORANGE);
+        readFortuneBtn.setOpaque(true);
+        readFortuneBtn.setBorderPainted(false);
+        quitBtn.setBackground(Color.RED);
+        quitBtn.setOpaque(true);
+        quitBtn.setBorderPainted(false);
         bottomPnl.add(readFortuneBtn);
         bottomPnl.add(quitBtn);
 
         mainPnl.add(bottomPnl, BorderLayout.SOUTH);
+        bottomPnl.setBackground(new Color(173,216,230));
 
     }
 
